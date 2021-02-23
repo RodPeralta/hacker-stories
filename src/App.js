08b96ef73616496fc21c4febe89b5edceab7c1ef
Rodrigo Peralta <rodrigo.p2002@gmail.com>
2021-02-23 16:47:46 -0300

    Adding Lists

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://reduc.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 const welcome = {
   greeting: 'Hey',
   title: 'React',
@@ -9,11 +28,26 @@ function App() {
   return (
     <div>
       <h1>
-        {welcome.greeting} {welcome.title}
+        My Hacker Stories
       </h1>
 
       <label htmlFor="search">Search: </label>
       <input id="search" type="text"/>
+
+      <hr/>
+
+      {list.map(function(item) {
+        return ( 
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.points}</span>
+          </div>
+        );
+      })}
     </div>
   );
 }
