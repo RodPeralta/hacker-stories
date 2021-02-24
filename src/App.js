8da0672141c2073b5a55a0e170c3934ad264b9e4
Rodrigo Peralta <rodrigo.p2002@gmail.com>
2021-02-24 11:48:50 -0300

    Adding Conditional Rendering with Load & Error

--- src/App.js
+++ src/App.js
@@ -48,11 +48,17 @@ const  App = () => {
   const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
 
   const [stories, setStories] = React.useState([]);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [isError, setIsError] = React.useState(false);
 
   React.useEffect(() => {
+    setIsLoading(true);
+
     getAsyncStories().then(result => {
       setStories(result.data.stories);
-    });
+      setIsLoading(false);
+    })
+    .catch(() => setIsError(true));
   },[]);
 
   const handleRemoveStory = item => {
@@ -83,7 +89,14 @@ const  App = () => {
 
       <hr/>
 
-      <List list={searchedStories} onRemoveItem={handleRemoveStory}/>
+      {isError && <p>Something went wrong...</p>}
+
+      {isLoading ? (
+        <p>Loading...</p>
+      ): (
+        <List list={searchedStories} onRemoveItem={handleRemoveStory}/>
+      )}
+      
       
     </div>
   );
