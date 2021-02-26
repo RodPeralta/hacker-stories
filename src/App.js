b886c25e4ca9ad202b4123dd76e2a591b2bafa8a
Rodrigo Peralta <rodrigo.p2002@gmail.com>
2021-02-26 11:37:09 -0300

    Adding svg files and reverse search functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import {sortBy} from 'lodash';
 
 import './App.css'
 import {ReactComponent as Check} from './check.svg';
+import {ReactComponent as DownArrow} from './downArrow.svg';
+import {ReactComponent as UpArrow} from './upArrow.svg';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -166,54 +168,67 @@ const SORTS = {
   NONE: list => list,
   TITLE: list => sortBy(list, 'title'),
   AUTHOR: list => sortBy(list, 'author'),
-  COMMENT: list => sortBy(list, 'num_comments').reverse(),
-  POINT: list => sortBy(list, 'points').reverse(),
+  COMMENT: list => sortBy(list, 'num_comments'),
+  POINT: list => sortBy(list, 'points'),
 
 };
 
 
 const List = ({list, onRemoveItem}) => {
-  const [sort, setSort] = React.useState('NONE');
+  const [sort, setSort] = React.useState({
+    sortKey: 'NONE',
+    isReverse: false,
+  });
 
   const handleSort = sortKey => {
-    setSort(sortKey);
+    const isReverse = sort.sortKey === sortKey && !sort.isReverse;
+    setSort({sortKey, isReverse});
   };
 
-  const sortFunction = SORTS[sort];
-  const sortedList = sortFunction(list);
+  const sortFunction = SORTS[sort.sortKey];
+  const sortedList = sort.isReverse ? sortFunction(list).reverse() : sortFunction(list);
 
   return (
     <div>
       <div className="item item_title">
         <span style={{width: '40% '}}>
-          <button type="button" onClick={() => handleSort('TITLE')} className="button button_title" 
-          style={sort !== 'TITLE' ? {backgroundColor: 'transparent'} : {backgroundColor: 'white'}}>
+          <button type="button" onClick={() => handleSort('TITLE')} className="button button_title">
             Title
           </button>
+          <SortingArrow descending={sort.isReverse} isSorted={sort.sortKey==='TITLE'}/>
         </span>
         <span style={{width: '30%'}}>
-          <button type="button" onClick={() => handleSort('AUTHOR')} className="button button_title"
-          style={sort !== 'AUTHOR' ? {backgroundColor: 'transparent'} : {backgroundColor: 'white'}}>
+          <button type="button" onClick={() => handleSort('AUTHOR')} className="button button_title">
             Author
           </button>
+          <SortingArrow descending={sort.isReverse} isSorted={sort.sortKey==='AUTHOR'}/>
         </span>
         <span style={{width: '10% ', textAlign: "center"}}>
-          <button type="button" onClick={() => handleSort('COMMENT')} className="button button_title"
-          style={sort !== 'COMMENT' ? {backgroundColor: 'transparent'} : {backgroundColor: 'white'}}>
+          <button type="button" onClick={() => handleSort('COMMENT')} className="button button_title">
             Comments
           </button>
+          <SortingArrow descending={sort.isReverse} isSorted={sort.sortKey==='COMMENT'}/>
         </span>
         <span style={{width: '10% ', textAlign: "center"}}>
-          <button type="button" onClick={() => handleSort('POINT')} className="button button_title"
-          style={sort !== 'POINT' ? {backgroundColor: 'transparent'} : {backgroundColor: 'white'}}>
+          <button type="button" onClick={() => handleSort('POINT')} className="button button_title">
             Points
           </button>
+          <SortingArrow descending={sort.isReverse} isSorted={sort.sortKey==='POINT'}/>
         </span>
         <span style={{width: '10% ', textAlign: "center"}}>Dismiss</span>
       </div> 
       {sortedList.map(item => <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem}/>)}
     </div>
   );
+};
+
+const SortingArrow = ({descending, isSorted}) => {
+  if (!isSorted) return null;
+  if (descending) {
+    return <DownArrow height="18px" width="18px" className="arrow"/>
+  } else {
+    return <UpArrow height="18px" width="18px" className="arrow"/>
+  }
 };
 
 const Item = ({item, onRemoveItem}) => (
